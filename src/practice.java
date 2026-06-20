f093b27f0f9b0d9b12dee737540be280ee689683
Tian Dong <tiandongny@gmail.com>
2026-06-20 16:15:45 -0400

    6/20

---
+++ src/practice.java
@@ -0,0 +1,10 @@
+public class practice {
+     public static int func(int x, int y){
+         return x+y;
+     }
+     int index = 0;
+     for (int i: nums){
+         System.out.println(index);
+         index++;
+    }
+}
