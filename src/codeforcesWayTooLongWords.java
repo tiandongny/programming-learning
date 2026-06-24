973e2b1de6a78be8f749cbd37588f6c6cad5b6c4
Tian Dong <tiandongny@gmail.com>
2026-06-24 11:23:39 -0400

    codeforces 71a

---
+++ src/codeforcesWayTooLongWords.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class codeforcesWayTooLongWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i=0; i<n; i++) {
+            String word = sc.next();
+            int len = word.length();
+            if (len > 10) {
+                System.out.println("" + word.charAt(0) + (len - 2) + word.charAt(len - 1));
+            } else {
+                System.out.println(word);
+            }
+        }
+    }
+}
